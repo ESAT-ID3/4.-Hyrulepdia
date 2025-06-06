20980ed4d6bc9af0ec0d8b23597a8a62be46f7e8
Joan-02 <>
2025-06-06 17:58:12 +0200

    Update ProfilePage.tsx

--- src/views/ProfilePage.tsx
+++ src/views/ProfilePage.tsx
@@ -1,10 +1,13 @@
 import { UserProfileEditor } from '../components/firebase/user-profile/UserProfileEditor';
+import { CardContainer } from '../components/CardContainer/CardContainer';
 
 //<UserProfileEditor />
 export const PorfilePage = () => {
   return (
     <div>
+      <CardContainer/>
       <UserProfileEditor />
     </div>
   );
 };
+
